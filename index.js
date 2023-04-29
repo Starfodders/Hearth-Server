7b67854c76f2a84e0afc000baa945865252132f3
Michael Deng <michaeldeng55@gmail.com>
2023-04-29 14:54:46 -0400

    hold off on https setup

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config();
 
+// const https = require('https')
+// const fs = require('fs')
+
+//self-assigning key for now, ran openSSL cmd
+// const options = {
+//     key: fs.readFileSync('./key.pem'),
+//     cert: fs.readFileSync('./cert.pem')
+//   };
+
 const PORT = process.env.PORT;
 const CLIENT_URL = process.env.CLIENT_URL;
 
@@ -26,8 +35,12 @@ app.use('/collections', collectionRoutes)
 app.use('/meditate', meditateRoutes)
 
 
+// const server = https.createServer(options, app)
 
+// server.listen(PORT, () => {
+//     console.log(`Listening on ${PORT}`);
+// })
 
 app.listen(PORT, () => {
-    console.log(`Listening on ${PORT}`);
+    console.log('Listening on ' + PORT);
 })
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const router = express.Router()
-const knex = require('knex')(require('../knexfile'));
 const userController = require('../controllers/userController')
 
 router.route('/signup').post(userController.signup)
